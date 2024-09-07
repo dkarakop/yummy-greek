d79aaa36746c0beae7d1e7fe5af49568cf99b676
Dimitra Karakopoulou <dkarakop@gmail.com>
2024-09-07 15:54:40 +0300

    Create MenuList component

---
+++ src/components/MenuList.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  dishes: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <li
+    class="flex justify-between items-center bg-orange-200"
+    v-for="dish in props.dishes"
+    :key="dish.id"
+  >
+    <img :src="dish.image" :alt="dish.name" />
+    <p>{{ dish.name }}</p>
+    <p>{{ dish.price }} €</p>
+    <button class="px-5 py-0.3 bg-yellow-600 rounded">Details</button>
+    <button class="px-5 py-0.3 bg-yellow-600 rounded">Add to Card</button>
+  </li>
+</template>
