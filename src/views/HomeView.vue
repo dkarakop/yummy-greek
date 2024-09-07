2dbdf58077761c3d2799eca2d8e5f3ba9ae2c9ff
Dimitra Karakopoulou <dkarakop@gmail.com>
2024-09-07 15:48:24 +0300

    Add HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>YummyGreek</h1>
+    <img src="../assets/logo.svg" alt="business-log" style="width: 300px" />
+    <p>The gourmands order before...they get hungry!</p>
+    <img src="" alt="Award IMG" />
+    <RouterLink to="/menu">Order Now</RouterLink>
   </main>
 </template>
