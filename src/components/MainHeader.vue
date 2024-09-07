cd5b595524e63f5067be4a8e2d6a5dd0d6011be5
Dimitra Karakopoulou <dkarakop@gmail.com>
2024-09-07 15:53:37 +0300

    Create MainHeader component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <header class="staic inset-x-0 top-0 flex justify-between bg-blue-500 text-white">
+    <img src="../assets/logo.svg" alt="business-log" style="width: 50px" />
+    <slot></slot>
+    <button class="px-5 py-0.3 bg-yellow-600 rounded">Cart</button>
+  </header>
+</template>
+
+<style scoped></style>
