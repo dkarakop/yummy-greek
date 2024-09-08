6a3a85ac6cdaa5d99ff8eac52c501e17e4ce1f9a
Dimitra Karakopoulou <dkarakop@gmail.com>
2024-09-08 12:57:38 +0300

    Add DishModal & use ModalWindow in FiltersModal

---
+++ src/components/DishModal.vue
@@ -0,0 +1,46 @@
+<script setup>
+import ModalWindow from '@/components/ModalWindow.vue'
+
+const props = defineProps({
+  dish: { type: Object, required: true }
+})
+</script>
+
+<template>
+  <ModalWindow>
+    <div>
+      <img :src="props.dish.image" :alt="'A picture of our delicious dish ' + props.name" />
+      <h2>{{ props.dish.name }}</h2>
+      <p>{{ 'Price: ' + props.dish.price + '€' }}</p>
+      <p>{{ props.dish.description }}</p>
+      <div>
+        <h3>Ingredients</h3>
+        <ul>
+          <li v-for="(ingredient, index) in props.dish.ingredients" :key="ingredient + '-' + index">
+            {{ ingredient }}
+          </li>
+        </ul>
+        <div>
+          <div v-if="props.dish?.tags?.dietaryPreferences.length !== 0">
+            <div
+              v-for="(tag, index) in props.dish.tags.dietaryPreferences"
+              :key="tag + '-' + index"
+              class="bg-green-700"
+            >
+              {{ tag }}
+            </div>
+          </div>
+          <div v-if="props.dish?.tags?.allergens.length !== 0">
+            <div
+              v-for="(tag, index) in props.dish.tags.allergens"
+              :key="tag + '-' + index"
+              class="bg-red-600"
+            >
+              {{ tag }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </ModalWindow>
+</template>
--- src/components/FiltersModal.vue
+++ src/components/FiltersModal.vue
@@ -1,14 +1,8 @@
 <script setup>
 import GenericCheckbox from './GenericCheckbox.vue'
+import GenericRadio from './GenericRadio.vue'
 import { reactive } from 'vue'
-const props = defineProps({
-  isVisible: { type: Boolean, required: true }
-})
-
-const emit = defineEmits(['close'])
-const closeFilterModal = () => {
-  emit('close')
-}
+import ModalWindow from './ModalWindow.vue'
 
 const filters = reactive({
   // Dietary Preferences
@@ -21,75 +15,61 @@ const filters = reactive({
   glutenAllergy: false,
   lasctoseAllergy: false,
   otherAllergens: false,
-  allergenFree: false
+  allergenFree: false,
+  //PriceRange
+  priceRange: null
 })
+
+const priceOptions = [
+  { label: 'Under €10', value: 'under10' },
+  { label: '€10 - €20', value: '10to20' },
+  { label: '€20 - €30', value: '20to30' },
+  { label: '€30 - €40', value: '30to40' },
+  { label: 'Over €40', value: 'over40' }
+]
 </script>
 
 <template>
-  <Teleport to="body">
-    <transition>
-      <div
-        v-if="props.isVisible"
-        class="fixed inset-0 bg-gray-500 bg-opacity-75 flex items-center justify-center"
-      >
-        <div class="bg-white rounded-lg shadow-lg p-6 relative w-10/12">
-          <!-- Modal Header -->
-          <div class="flex justify-between items-center">
-            <h2 class="text-2xl text-center font-bold">Filters</h2>
-            <button @click="closeFilterModal" class="text-gray-400 hover:text-gray-500">✕</button>
-          </div>
+  <ModalWindow modalName="Filters">
+    <!-- Modal Body -->
+    <div class="flex gap-6">
+      <!-- Dietary Preferences Checkbox-->
+      <fieldset class="mt-4 border border-gray-300 p-4 rounded-md grow">
+        <legend class="text-lg font-semibold text-slate-800">Dietary Preferences</legend>
+        <GenericCheckbox label="Vegan" v-model="filters.vegan"></GenericCheckbox>
+        <GenericCheckbox label="Gluten-Free" v-model="filters.glutenFree"></GenericCheckbox>
+        <GenericCheckbox label="Lactose-Free" v-model="filters.lactoseFree"></GenericCheckbox>
+        <GenericCheckbox label="Nut-Free" v-model="filters.nutFree"></GenericCheckbox>
+      </fieldset>
 
-          <!-- Modal Body -->
-          <!-- Dietary Preferences Checkbox-->
-          <div class="mt-4">
-            <h2 class="text-xl font-bold">Dietary Preferences</h2>
-            <GenericCheckbox label="Vegan" v-model="filters.vegan"></GenericCheckbox>
-            <GenericCheckbox label="Gluten-Free" v-model="filters.glutenFree"></GenericCheckbox>
-            <GenericCheckbox label="Lactose-Free" v-model="filters.lactoseFree"></GenericCheckbox>
-            <GenericCheckbox label="Nut-Free" v-model="filters.nutFree"></GenericCheckbox>
-          </div>
+      <!-- Allergens Checkbox-->
+      <fieldset class="mt-4 border border-gray-300 p-4 rounded-md grow">
+        <legend class="text-lg font-semibold text-slate-800">Allergens</legend>
+        <GenericCheckbox label="Contains Nuts" v-model="filters.nutsAllergy"></GenericCheckbox>
+        <GenericCheckbox label="Contains Gluten" v-model="filters.glutenAllergy"></GenericCheckbox>
+        <GenericCheckbox
+          label="Contains Lactose"
+          v-model="filters.lasctoseAllergy"
+        ></GenericCheckbox>
+        <GenericCheckbox label="Other Allergens" v-model="filters.otherAllergens"></GenericCheckbox>
+        <GenericCheckbox label="Allergen-Free" v-model="filters.allergenFree"></GenericCheckbox>
+      </fieldset>
 
-          <!-- Allergens Checkbox-->
-          <div class="mt-4">
-            <h2 class="text-xl font-bold">Allergens</h2>
-            <GenericCheckbox label="Contains Nuts" v-model="filters.nutsAllergy"></GenericCheckbox>
-            <GenericCheckbox
-              label="Contains Gluten"
-              v-model="filters.glutenAllergy"
-            ></GenericCheckbox>
-            <GenericCheckbox
-              label="Contains Lactose"
-              v-model="filters.lasctoseAllergy"
-            ></GenericCheckbox>
-            <GenericCheckbox
-              label="Other Allergens"
-              v-model="filters.otherAllergens"
-            ></GenericCheckbox>
-            <GenericCheckbox label="Allergen-Free" v-model="filters.allergenFree"></GenericCheckbox>
-          </div>
-
-          <!-- Modal Footer -->
-          <div class="mt-6 flex justify-end">
-            <button
-              @click="closeFilterModal"
-              class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-            >
-              Close
-            </button>
-          </div>
-        </div>
-      </div>
-    </transition>
-  </Teleport>
+      <!-- Price Range -->
+      <GenericRadio
+        :options="priceOptions"
+        name="priceRange"
+        v-model="filters.priceRange"
+      ></GenericRadio>
+    </div>
+    <!-- Modal Footer -->
+    <div class="mt-6 flex justify-between">
+      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Reset Filters
+      </button>
+      <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+        Apply Filters
+      </button>
+    </div>
+  </ModalWindow>
 </template>
-
-<style>
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.3s ease;
-}
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
-</style>
