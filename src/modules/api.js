353bf967bcf0fca4cef85043919fe9b4def33287
Dimitra Karakopoulou <dkarakop@gmail.com>
2024-09-10 11:39:43 +0300

    Create processedTags property for each dish in api.js file

--- src/modules/api.js
+++ src/modules/api.js
@@ -6,5 +6,51 @@ const apiUrl = 'http://localhost:3000'
  * @returns a Promise
  */
 export function getDishes() {
-  return fetch(`${apiUrl}/dishes`).then((response) => response.json())
+  return fetch(`${apiUrl}/dishes`)
+    .then((response) => response.json())
+    .then((json) => {
+      json.forEach((dish) => processTags(dish))
+      return json
+    })
+}
+
+function processTags(dish) {
+  const processedTags = {
+    vegan: false,
+    vegetarian: false,
+    gluten: false,
+    lactose: false,
+    nuts: false,
+    other: false,
+    allergenFree: false
+  }
+
+  const allTags = dish.tags.dietaryPreferences.concat(dish.tags.allergens)
+
+  processedTags.vegan = allTags.includes('vegan')
+  processedTags.vegetarian = allTags.includes('vegetarian')
+  processedTags.gluten = !allTags.includes('gluten-free')
+  processedTags.lactose = !allTags.includes('lactose-free')
+  processedTags.nuts = allTags.includes('contains-nuts')
+
+  processedTags.allergenFree =
+    !allTags.includes('contains-nuts') &&
+    allTags.includes('lactose-free') &&
+    allTags.includes('gluten-free')
+
+  allTags.forEach((tag) => {
+    const tags = [
+      'vegan',
+      'vegetarian',
+      'contains-nuts',
+      'contains-gluten',
+      'lactose-free',
+      'gluten-free'
+    ]
+    if (!tags.includes(tag)) {
+      processedTags.other = true
+    }
+  })
+
+  dish.processedTags = processedTags
 }
