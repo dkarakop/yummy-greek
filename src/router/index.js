11070945283a4ba1dd54a2d5249cff2e8f90e27c
Dimitra Karakopoulou <126284048+dkarakop@users.noreply.github.com>
2024-09-13 18:35:57 +0300

    Add scroll behavior between views

--- src/router/index.js
+++ src/router/index.js
@@ -16,8 +16,27 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/MenuView.vue')
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: () => import('../views/CartView.vue')
+    },
+    {
+      path: '/confirmation',
+      name: 'order confirmation',
+
+      component: () => import('../views/OrderConfirmationView.vue')
     }
-  ]
+  ],
+  // Always scroll to top
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  }
 })
 
 export default router
