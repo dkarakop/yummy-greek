9b80cad07a9a72bc197e1fc6d124ee1d05b00856
Dimitra Karakopoulou <126284048+dkarakop@users.noreply.github.com>
2024-09-13 15:52:50 +0300

    Display cart button only when items are added

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,19 +1,24 @@
 <script setup>
 import { RouterLink } from 'vue-router'
 import { useCartStore } from '../stores/cart.js'
+
 //Store
 const cartStore = useCartStore()
 </script>
 
 <template>
   <header class="fixed top-0 left-0 w-full bg-darkBlue py-4 px-8 shadow-lg z-50">
-    <div class="container mx-auto flex justify-between items-center">
-      <div class="flex items-center space-x-4">
+    <div
+      class="container mx-auto flex flex-col md:flex-row justify-between items-center space-y-4 md:space-y-0"
+    >
+      <div
+        class="flex flex-col md:flex-row items-center space-x-0 md:space-x-4 space-y-2 md:space-y-0"
+      >
         <RouterLink to="/">
           <img
             src="./icons/yummy-greek-logo.png"
             alt="An illustrated Souvlaki logo with olive branches and Greek patterns"
-            class="w-20 h-auto p-1 shadow-2xl rounded-lg md:w-16 focus:outline-none focus:ring-2 focus:ring-yellow"
+            class="w-16 h-auto p-1 shadow-2xl rounded-lg focus:outline-none focus:ring-2 focus:ring-yellow"
             aria-label="Go to homepage"
             tabindex="0"
             role="button"
@@ -23,15 +28,21 @@ const cartStore = useCartStore()
         <h1 class="text text-xl font-bold text-yellow">YummyGreek</h1>
       </div>
 
-      <RouterLink to="/cart" class="btn btn__cart btn__cart--interactions">
+      <RouterLink
+        v-if="cartStore.totalAmountDishes !== 0"
+        to="/cart"
+        class="btn btn__cart btn__cart--interactions w-full md:w-auto flex justify-center items-center space-x-4"
+        title="Go to your order"
+      >
         <span
-          class="bg-white rounded-lg px-4 py-2 text-base mr-4 hover:bg-white text-black underline-none focus:bg-white focus:text-black"
+          class="bg-white rounded-lg px-4 py-2 text-base hover:bg-white text-black no-underline focus:bg-white focus:text-black"
           title="Total number of dishes in your cart"
         >
-          {{ cartStore.totalAmountDishes }}</span
-        >See your Cart
-        <span class="text-lg">
-          {{ cartStore.totalPrice == 0.0 ? '' : '€' + cartStore.totalPrice }}</span
+          {{ cartStore.totalAmountDishes }}
+        </span>
+        <span class="text-base"
+          >See your Cart
+          <strong>{{ cartStore.totalPrice == 0.0 ? '' : '€' + cartStore.totalPrice }}</strong></span
         >
       </RouterLink>
     </div>
